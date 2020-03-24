27d88fff55504107fe5c75eb56afeeb975298116
Anmol Verma <anmol.verma4@gmail.com>
2020-03-24 21:12:10 +0530

    As a concerned user, I want to know how and when I can get tested, so that I can confirm whether or not I have COVID-19.

--- lib/ui/screens/UserInfoCollectorScreen.dart
+++ lib/ui/screens/UserInfoCollectorScreen.dart
@@ -1,4 +1,5 @@
 import 'package:corona_trace/APIRepository.dart';
+import 'package:corona_trace/AppConstants.dart';
 import 'package:corona_trace/ui/BaseState.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -14,21 +15,19 @@ class UserInfoCollectorScreen extends StatefulWidget {
       _UserInfoCollectorScreenState();
 }
 
-const DISTANCE_DISPLACEMENT_FACTOR = "DISTANCE_DISPLACEMENT_FACTOR";
+const SCREEN_FEELING_TODAY = 1;
+const SCREEN_ACKNOWLEDGEMENT = 2;
+const SCREEN_TESTING_INFORMATION = 3;
 
 class _UserInfoCollectorScreenState extends BaseState<UserInfoCollectorScreen> {
-  int _currentScreen = 1;
-
-  @override
-  void initState() {
-    super.initState();
-  }
+  int _currentScreen = SCREEN_FEELING_TODAY;
 
   @override
   Widget prepareWidget(BuildContext context) {
     return Scaffold(
       key: scaffoldKey,
       body: SafeArea(
+        top: false,
         bottom: false,
         child: safeAreaContainer(),
       ),
@@ -36,38 +35,54 @@ class _UserInfoCollectorScreenState extends BaseState<UserInfoCollectorScreen> {
     );
   }
 
-  Stack safeAreaContainer() {
-    return Stack(
+  Widget safeAreaContainer() {
+    return Column(
       children: <Widget>[
-        Container(
-          child: CTHeaderTile(),
-          margin: EdgeInsets.only(top: 50, bottom: 20),
-        ),
-        Align(
-          child: Padding(
-            child: Image.asset("assets/combined_shape.png"),
-            padding: EdgeInsets.only(top: 20, bottom: 50),
-          ),
-          alignment: Alignment.topRight,
-        ),
-        Align(
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 200,
-                ),
-                getCardBodySeverity()
-              ],
+        Stack(
+          children: <Widget>[
+            SizedBox(height: 10,),
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  backIcon(),
+                  CTHeaderTile(),
+                  testingInformation()
+                ],
+              ),
+              margin: EdgeInsets.only(top: 50, bottom: 20),
             ),
-          ),
-          alignment: Alignment.bottomCenter,
+            Align(
+              child: Padding(
+                child: Image.asset("assets/combined_shape.png"),
+                padding: EdgeInsets.only(top: 20, bottom: 50),
+              ),
+              alignment: Alignment.topRight,
+            ),
+          ],
         ),
+        Expanded(
+          flex: 1,
+          child: Align(
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 200,
+                  ),
+                  getCardBodySeverity()
+                ],
+              ),
+            ),
+            alignment: Alignment.bottomCenter,
+          ),
+        )
       ],
     );
   }
 
   Card getCardBodySeverity() {
+    Widget currentScreen = getCurrentScreen();
     return Card(
       margin: EdgeInsets.all(0),
       shape: RoundedRectangleBorder(
@@ -75,7 +90,7 @@ class _UserInfoCollectorScreenState extends BaseState<UserInfoCollectorScreen> {
               topLeft: Radius.circular(10), topRight: Radius.circular(10))),
       child: Padding(
         padding: EdgeInsets.only(left: 20, right: 20, top: 30, bottom: 50),
-        child: _currentScreen == 1 ? firstCardContent() : secondCardContent(),
+        child: currentScreen,
       ),
     );
   }
@@ -85,11 +100,11 @@ class _UserInfoCollectorScreenState extends BaseState<UserInfoCollectorScreen> {
         positiveQuestionText: "I am not infected with Coronavirus",
         negativeQuestionText: "I am infected with Coronavirus",
         onNegativeQuestionClick: () async {
-          dialogOnResponse(2);
+          dialogOnResponse(SCREEN_ACKNOWLEDGEMENT);
           FirestoreRepository.setUserSeverity(1);
         },
         onPositiveQuestionClick: () async {
-          dialogOnResponse(2);
+          dialogOnResponse(SCREEN_ACKNOWLEDGEMENT);
           FirestoreRepository.setUserSeverity(0);
         });
 
@@ -103,23 +118,139 @@ class _UserInfoCollectorScreenState extends BaseState<UserInfoCollectorScreen> {
           String url = await FirestoreRepository.getRemoteConfigValue(key);
           print(url);
           hideLoadingDialog();
-          if (await canLaunch(url)) {
-            await launch(url);
-          }
+          AppConstants.launchUrl(url);
         });
   }
 
   Widget secondCardContent() {
     return CTThankYouDialog(
       onButtonClick: () {
-        dialogOnResponse(1);
+        dialogOnResponse(SCREEN_FEELING_TODAY);
       },
     );
   }
 
-  void dialogOnResponse(screen) async {
+  Widget testingInformationCardContent() {
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          height: 20,
+        ),
+        Container(
+          decoration: new BoxDecoration(
+              color: Colors.transparent,
+              border: Border.all(color: Colors.indigo),
+              borderRadius: new BorderRadius.all(const Radius.circular(8.0))),
+          child: ListTile(
+            onTap: () {
+              AppConstants.launchUrl(AppConstants.TESTED_URL);
+            },
+            trailing: Icon(Icons.navigate_next, color: Colors.indigo),
+            title: Padding(
+              child: Text(
+                "How to get tested",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              padding: EdgeInsets.all(5),
+            ),
+            subtitle: Padding(
+              child: Text("COVID-19 Testing Resources"),
+              padding: EdgeInsets.all(5),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Container(
+          decoration: new BoxDecoration(
+              color: Colors.transparent,
+              border: Border.all(color: Colors.indigo),
+              borderRadius: new BorderRadius.all(const Radius.circular(8.0))),
+          child: ListTile(
+            onTap: () {
+              AppConstants.launchUrl(AppConstants.TESTED_URL);
+            },
+            trailing: Icon(Icons.navigate_next, color: Colors.indigo),
+            title: Padding(
+              child: Text("How to get documentation",
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              padding: EdgeInsets.all(5),
+            ),
+            subtitle: Padding(
+              child: Text("COVID-19 Testing Documentation Resources"),
+              padding: EdgeInsets.all(5),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 120,
+        ),
+      ],
+    );
+  }
+
+  void dialogOnResponse(screen) {
     setState(() {
       _currentScreen = screen;
     });
+  }
+
+  testingInformation() {
+    return _currentScreen != SCREEN_TESTING_INFORMATION
+        ? Container(
+            child: FlatButton(
+              shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(8.0),
+                  side: BorderSide(color: Colors.indigo.shade300)),
+              color: Colors.transparent,
+              textColor: Colors.indigo,
+              padding: EdgeInsets.all(8.0),
+              onPressed: () {
+                dialogOnResponse(SCREEN_TESTING_INFORMATION);
+              },
+              child: Text(
+                "Testing Information",
+                style: TextStyle(fontSize: 14.0, color: Colors.white),
+              ),
+            ),
+            margin: EdgeInsets.only(left: 20),
+          )
+        : Container();
+  }
+
+  backIcon() {
+    return _currentScreen == SCREEN_TESTING_INFORMATION
+        ? IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              dialogOnResponse(SCREEN_FEELING_TODAY);
+            },
+          )
+        : Container();
+  }
+
+  Widget getCurrentScreen() {
+    switch (_currentScreen) {
+      case SCREEN_TESTING_INFORMATION:
+        {
+          return testingInformationCardContent();
+        }
+        break;
+      case SCREEN_FEELING_TODAY:
+        {
+          return firstCardContent();
+        }
+        break;
+      case SCREEN_ACKNOWLEDGEMENT:
+        {
+          return secondCardContent();
+        }
+        break;
+    }
+    return Container();
   }
 }
