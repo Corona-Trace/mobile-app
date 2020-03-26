5d2539660bb0ee9cb26e17d47d4308b8872bb8d4
Anmol Verma <anmol.verma4@gmail.com>
2020-03-26 14:58:54 +0530

    UI created for list and map notification detail

--- lib/ui/notifications/NotificationsListScreen.dart
+++ lib/ui/notifications/NotificationsListScreen.dart
@@ -56,44 +56,93 @@ class _NotificationsListScreenState extends State<NotificationsListScreen> {
                     SizedBox(
                       height: 10,
                     ),
-                    Container(
-                      color: Color.fromRGBO(241, 227, 178, 1),
-                      child: Padding(
-                        padding: EdgeInsets.only(
-                            top: 20, bottom: 20, left: 16, right: 16),
-                        child: Text(
-                            "Please updated your status if you test positive for COVID-19."),
-                      ),
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Container(
-                          child: ListTile(
-                            title: Text(
-                              "No Symptoms",
-                              style: TextStyle(
-                                  color: Colors.green,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            subtitle: Text("Your Anonymous Status"),
-                            leading: Icon(
-                              Icons.remove_circle_outline,
-                              color: Colors.green,
-                            ),
-                          ),
-                          height: 100,
-                          width: MediaQuery.of(context).size.width * 0.75,
-                        ),
-                        Row()
-                      ],
-                    ),
+                    notificationHistory(),
+                    updateYourStatus(),
+                    noSymptomsUpdate(context),
                   ],
                 ),
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Row noSymptomsUpdate(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          child: ListTile(
+            title: Text(
+              "No Symptoms",
+              style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text("Your Anonymous Status"),
+            leading: Icon(
+              Icons.remove_circle_outline,
+              color: Colors.green,
+            ),
+          ),
+          height: 110,
+          width: MediaQuery.of(context).size.width * 0.75,
+        ),
+        Container(
+          margin: EdgeInsets.only(bottom: 30),
+          child: Row(
+            children: <Widget>[
+              Text(
+                "Update",
+                style: TextStyle(
+                    color: Colors.indigo,
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold),
+              ),
+              Icon(
+                Icons.arrow_forward,
+                color: Colors.indigo,
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  Container updateYourStatus() {
+    return Container(
+      margin: EdgeInsets.all(0),
+      padding: EdgeInsets.all(0),
+      color: Color.fromRGBO(241, 227, 178, 1),
+      child: Padding(
+        padding: EdgeInsets.only(top: 20, bottom: 20, left: 16, right: 16),
+        child: Text(
+            "Please updated your status if you test positive for COVID-19."),
+      ),
+    );
+  }
+
+  Container notificationHistory() {
+    return Container(
+      margin: EdgeInsets.only(bottom: 10, top: 10, right: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Text(
+            "Notification History",
+            style: TextStyle(
+                color: Colors.indigo,
+                fontSize: 17,
+                fontWeight: FontWeight.bold),
+          ),
+          Icon(
+            Icons.arrow_forward,
+            color: Colors.indigo,
+          )
+        ],
       ),
     );
   }
