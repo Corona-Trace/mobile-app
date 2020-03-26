3f223408f531e197fcdfe0e4dcd0028e443ef9a1
Anmol Verma <anmol.verma4@gmail.com>
2020-03-26 18:18:41 +0530

    updated UI header with new designs

--- lib/ui/widgets/CTThankYouDialog.dart
+++ lib/ui/widgets/CTThankYouDialog.dart
@@ -1,6 +1,9 @@
+import 'package:corona_trace/main.dart';
+import 'package:corona_trace/ui/widgets/CTTermsAndConditions.dart';
 import 'package:flutter/material.dart';
 
 class CTThankYouDialog extends StatelessWidget {
+
   CTThankYouDialog({this.onButtonClick});
 
   final Function onButtonClick;
@@ -15,7 +18,9 @@ class CTThankYouDialog extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              SizedBox(height: 30.0,),
+              SizedBox(
+                height: 30.0,
+              ),
               Text(
                 "Thank you for your response!",
                 style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
@@ -24,7 +29,7 @@ class CTThankYouDialog extends StatelessWidget {
                 height: 10,
               ),
               Text(
-                "Thank you for joining hands and helping humanity. Remember you can always comeback and submit an updated status.",
+                "Reporting your status is helping everyone stay safe and slowing the spread of COVID-19.\n\nYour location will be used anonymously to help stop the spread of COVID-19.\n\nRemember, we will never store or share any personal information. ",
                 style: TextStyle(fontSize: 16),
               ),
               SizedBox(
@@ -35,20 +40,22 @@ class CTThankYouDialog extends StatelessWidget {
                   width: double.infinity,
                   height: 50,
                   child: MaterialButton(
-                    color: Colors.white,
+                    color: Color.fromRGBO(43, 54, 181, 1),
                     shape: RoundedRectangleBorder(
                       borderRadius: new BorderRadius.circular(8.0),
-                      side: BorderSide(color: Colors.black),
                     ),
                     onPressed: onButtonClick,
                     child: Text(
-                      "Got It",
-                      style: TextStyle(color: Colors.black, fontSize: 18),
+                      "Ok",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
                     ),
                   ),
                 ),
                 alignment: Alignment.center,
-              )
+              ),
+              SizedBox(
+                height: 20,
+              ),
             ],
           ),
         ),
