eb9b71d06829ebab0c31c95bfd32542c30ce7ad5
Anmol Verma <anmol.verma4@gmail.com>
2020-03-26 19:31:26 +0530

    updated stack for navigating via back buttons on android.

---
+++ lib/utils/Stack.dart
@@ -0,0 +1,28 @@
+import 'dart:collection';
+
+class StackCollect<T> {
+  final ListQueue<T> _list = ListQueue();
+
+  /// check if the stack is empty.
+  bool get isEmpty => _list.isEmpty;
+
+  /// check if the stack is not empty.
+  bool get isNotEmpty => _list.isNotEmpty;
+
+  /// push element in top of the stack.
+  void push(T e) {
+    _list.addLast(e);
+  }
+
+  /// get the top of the stack and delete it.
+  T pop() {
+    T res = _list.last;
+    _list.removeLast();
+    return res;
+  }
+
+  /// get the top of the stack without deleting it.
+  T top() {
+    return _list.last;
+  }
+}
