b5fd702ae7eb608f2962a6f1d6b859bebdab983a
Anmol Verma <anmol.verma@mutualmobile.com>
2020-03-27 23:00:39 +0530

    fixed import

--- android/app/src/main/java/org/coronatrace/mobile/Application.java
+++ android/app/src/main/java/org/coronatrace/mobile/Application.java
@@ -4,6 +4,7 @@ import io.flutter.app.FlutterApplication;
 import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
 import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
+import com.transistorsoft.flutter.backgroundgeolocation.FLTBackgroundGeolocationPlugin;
 
 public class Application extends FlutterApplication implements PluginRegistry.PluginRegistrantCallback {
     @Override
