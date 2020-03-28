a4f8971bec6a2455b4cd6d6c6768ae9af3af5613
Anmol Verma <anmol.verma4@gmail.com>
2020-03-28 22:15:39 +0530

    fixed onboarding and ui text issues on smaller screens

--- lib/ui/screens/Onboarding.dart
+++ lib/ui/screens/Onboarding.dart
@@ -5,17 +5,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 final kMainTitleStyle = TextStyle(
-  color: Colors.white,
-  fontSize: 32.0,
+  color: Color.fromRGBO(227, 203, 228, 1),
+  fontSize: 28.0,
 );
 
 final kSubtitleStyle = TextStyle(
-  color: Colors.white,
+  color: Color.fromRGBO(227, 203, 228, 1),
   fontSize: 24.0,
 );
 
 final kMainTextStyle = TextStyle(
-  color: Colors.white,
+  color: Color.fromRGBO(227, 203, 228, 1),
   fontSize: 17.0,
 );
 
@@ -58,7 +58,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
         child: Container(
           color: Color(0xFF2c3054),
           child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 40.0),
+            padding: EdgeInsets.symmetric(vertical: 20.0),
             child: Column(
               mainAxisSize: MainAxisSize.max,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -69,6 +69,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                     physics: ClampingScrollPhysics(),
                     controller: _pageController,
                     onPageChanged: (int page) {
+                      print("pageChange");
                       setState(() {
                         _currentPage = page;
                       });
@@ -95,66 +96,77 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   }
 
   Widget getFirstPage() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Image.asset("assets/images/firstonboarding.png"),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 8.0),
-          child: Text(
-            AppLocalization.text("Onboarding.Stop.Spread"),
-            style: kMainTitleStyle,
-          ),
+    return SingleChildScrollView(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+            minHeight: MediaQuery.of(context).size.height * 0.85),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("assets/images/firstonboarding.png"),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 24.0, vertical: 8.0),
+              child: Text(
+                AppLocalization.text("Onboarding.Stop.Spread"),
+                style: kMainTitleStyle,
+              ),
+            ),
+            SizedBox(height: 15.0),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 24.0, vertical: 8.0),
+              child: Text(
+                AppLocalization.text("Onboarding.Single.Answer"),
+                style: kSubtitleStyle,
+              ),
+            ),
+            SizedBox(height: 15.0),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 24.0, vertical: 8.0),
+              child: Text(
+                AppLocalization.text("Onboarding.Anonymous"),
+                style: kMainTextStyle,
+              ),
+            ),
+          ],
         ),
-        SizedBox(height: 15.0),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 8.0),
-          child: Text(
-            AppLocalization.text("Onboarding.Single.Answer"),
-            style: kSubtitleStyle,
-          ),
-        ),
-        SizedBox(height: 15.0),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 8.0),
-          child: Text(
-            AppLocalization.text("Onboarding.Anonymous"),
-            style: kMainTextStyle,
-          ),
-        ),
-      ],
+      ),
     );
   }
 
   Widget getSecondPage() {
     return Padding(
-      padding: EdgeInsets.all(40.0),
+      padding: EdgeInsets.only(left: 20, right: 20),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Center(
+          Align(
             child: Image(
               image: AssetImage(
                 "assets/images/Illustration_Anonymous.png",
               ),
-              height: 300.0,
-              width: 300.0,
+              width: 200,
+              height: 200,
             ),
+            alignment: Alignment.center,
           ),
           Text(
             AppLocalization.text("Onboarding.CheckIn"),
+            textAlign: TextAlign.start,
             style: kSubtitleStyle,
           ),
           SizedBox(height: 15.0),
           Align(
             child: Text(
               AppLocalization.text("Onboarding.Help"),
-              textAlign: TextAlign.center,
+              textAlign: TextAlign.left,
               style: kMainTextStyle,
             ),
-            alignment: Alignment.center,
+            alignment: Alignment.centerLeft,
           ),
         ],
       ),
@@ -162,62 +174,82 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   }
 
   Widget getThirdPage() {
-    return Padding(
-      padding: EdgeInsets.all(40.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-              height: 8.0,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                color: Color(0xFFE3CBE4),
-              )),
-          SizedBox(height: 30.0),
-          Text(
-            AppLocalization.text("Onboarding.Notified"),
-            style: kSubtitleStyle,
-          ),
-          SizedBox(height: 15.0),
-          Text(
-            AppLocalization.text("Onboarding.Notified.CrossPaths"),
-            textAlign: TextAlign.center,
-            style: kMainTextStyle,
-          ),
-        ],
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.only(left: 20, right: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Color.fromRGBO(227, 203, 228, 1),
+                        Color.fromRGBO(227, 203, 228, 0.8),
+                        Color.fromRGBO(227, 203, 228, 0.6),
+                        Color.fromRGBO(227, 203, 228, 0.4),
+                        Color.fromRGBO(227, 203, 228, 0.2),
+                        Color.fromRGBO(227, 203, 228, 0)
+                      ],
+                    ),
+                  ),
+                  width: 8.0,
+                  height: 150,
+                )
+              ],
+            ),
+            SizedBox(height: 15.0),
+            Text(
+              AppLocalization.text("Onboarding.Notified"),
+              style: kSubtitleStyle,
+            ),
+            SizedBox(height: 15.0),
+            Text(
+              AppLocalization.text("Onboarding.Notified.CrossPaths"),
+              textAlign: TextAlign.left,
+              style: kMainTextStyle,
+            ),
+          ],
+        ),
       ),
     );
   }
 
   Widget getFourthPage() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Center(
-          child: Image(
-            image: AssetImage(
-              'assets/images/Illustration_Anonymous.png',
+    return Padding(
+      padding: EdgeInsets.only(left: 20, right: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Align(
+            child: Image(
+              image: AssetImage(
+                "assets/images/Illustration_Anonymous.png",
+              ),
+              width: 200,
+              height: 200,
             ),
-            height: 300.0,
-            width: 300.0,
+            alignment: Alignment.center,
           ),
-        ),
-        SizedBox(height: 10.0),
-        Text(
-          AppLocalization.text("Onboarding.Meaningful"),
-          style: kSubtitleStyle,
-        ),
-        SizedBox(height: 15.0),
-        Padding(
-          child: Text(
+          Text(
+            AppLocalization.text("Onboarding.Meaningful"),
+            style: kSubtitleStyle,
+          ),
+          SizedBox(height: 15.0),
+          Text(
             AppLocalization.text("Onboarding.Meaningful.Guidelines"),
             style: kMainTextStyle,
           ),
-          padding: EdgeInsets.only(left: 20, right: 20),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
@@ -241,6 +273,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
           buttonTitle = AppLocalization.text("Continue");
         }
     }
+    print(_currentPage);
     return Padding(
         padding: const EdgeInsets.fromLTRB(12.0, 36.0, 12.0, 0.0),
         child: MaterialButton(
@@ -249,12 +282,24 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             borderRadius: new BorderRadius.circular(8.0),
           ),
           color: Color(0xFF475df3),
-          child: Text(
-            buttonTitle,
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 20.0,
-                fontWeight: FontWeight.w500),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                buttonTitle,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w500),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Visibility(
+                child: Icon(Icons.arrow_forward, color: Colors.white),
+                visible: _currentPage == 0 || _currentPage == 3,
+              )
+            ],
           ),
           onPressed: () async {
             if (_currentPage < 3) {
