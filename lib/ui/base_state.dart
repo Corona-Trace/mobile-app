f3e7879d18c40aefdb1dbe54c94afeab5babcfef
Anmol Verma <anmol.verma4@gmail.com>
2020-04-03 11:10:37 +0530

    1. added screen names for analytics logging

--- lib/ui/base_state.dart
+++ lib/ui/base_state.dart
@@ -1,4 +1,6 @@
+import 'package:corona_trace/main.dart';
 import 'package:corona_trace/ui/notifications/ct_loader.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -7,12 +9,24 @@ abstract class BaseState<T extends StatefulWidget> extends State<T> {
   final GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
 
   @override
+  void initState() {
+    super.initState();
+    MyApp.analytics.setCurrentScreen(screenName: screenName()).catchError(
+      (Object error) {
+        debugPrint('$FirebaseAnalyticsObserver: $error');
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     ScreenUtil.init(context, width: 375, height: 812, allowFontScaling: false);
     return prepareWidget(context);
   }
 
   Widget prepareWidget(BuildContext context);
+
+  String screenName();
 
   showLoadingDialog({bool tapDismiss}) {
     if (!isVisible) {
